a23f312b755c6b17f1235d1d5a60cf9a66c2c4b8
Tez Park <tez.park@sendbird.com>
2025-03-22 01:59:17 +0900

    Updated chatSDK dependency for beta

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ let package = Package(
         .package(
             url: "https://github.com/sendbird/sendbird-ai-agent-core-ios",
             from: "0.0.6"
+        ),
+        .package(
+            url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
+            revision: "256c46a73e5d545e7e1ca4f98750e39d35c3cdbe"
         )
     ],
     targets: [
