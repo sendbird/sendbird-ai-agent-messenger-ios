453042ce121df13e2846fee98a56d296790a0dd4
distiller <sha.sdk_deployment@sendbird.com>
2025-06-30 18:00:40 +0000

    Release - 0.9.9

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/sendbird/sendbird-ai-agent-core-ios",
-            from: "0.9.8"
+            from: "0.9.9"
         )
     ],
     targets: [
